536a848f526040b7ea898d8ea3ceec16b0305195
Naman Pandey <2492004namanpandey@gmail.com>
2024-02-22 08:04:29 +0530

    Create search page for web and image

---
+++ src/app/search/layout.jsx
@@ -0,0 +1,14 @@
+import SearchHeader from '@/components/SearchHeader'
+import './../globals.css'
+
+
+const layout = ({ children }) => {
+    return (
+        <div>
+            <SearchHeader />
+            {children}
+        </div>
+    )
+}
+
+export default layout
